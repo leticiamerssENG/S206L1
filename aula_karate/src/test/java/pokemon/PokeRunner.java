0f2f45fe16c18d51eb4dc800197a2c98483ef952
Letícia Merss <69372437+leticiamerssENG@users.noreply.github.com>
2023-05-20 07:55:41 -0300

    Update PokeRunner.java

--- aula_karate/src/test/java/pokemon/PokeRunner.java
+++ aula_karate/src/test/java/pokemon/PokeRunner.java
@@ -1,11 +1,11 @@
-package aula_inatel.pokemon;
+package aula_inatel.star_wars.pokemon;
 
 import com.intuit.karate.junit5.Karate;
 
 class PokeRunner {
     
     @Karate.Test
-    Karate testStarWars() {
+    Karate testPokemon() {
         return Karate.run("pokemon").relativeTo(getClass());
     }    
 
